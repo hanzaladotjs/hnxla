d449965e303b571e9233f5c5890067d4b5a12657
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-07-13 01:32:42 +0530

    tired af

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,7 +1,17 @@
 const Blogs= () => {
     return (
-        <div>
-hi
+        <div className="flex flex-col ml-1 mt-10">
+            <div className="text-3xl mb-10">
+                Started writing blogs a couple of months ago, here are a few:
+            </div>
+
+            <div className="text-blue-500 text-2xl hover:font-semibold">
+               - <a href="https://medium.com/@hanzaladotjs/wth-is-web3-8d672eadfd1d" className="underline" >WTH is Web3?? </a>
+            </div>
+              <div className="text-blue-500 text-2xl hover:font-semibold">
+               - <a href="https://hanzalaa.medium.com/capitalism-its-principles-c38959bce184" className="underline" > Captilism and it's principles </a>
+            </div>
+
         </div>
     )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,22 @@
-const Projects = () => {
+import { AnimatedTestimonials } from "./ui/animated-testimonials"
+
+
+const Projects = ({light}:any) => {
+
+    const pow:any = [
+        {
+            name: "Quickfire",
+            designation: "Postman for kids",
+            quote: `Built a postman like app for absolute beginners, its has basic features like get, post, put, delete, headers.
+params, and body. Tech stack: Typescript, Reactjs (fetchapi), Context API, TailwindCSS.(not responsive yet)`,
+src: "/src/assets/quickfire.png",
+live: "https://quickfire.hanzala.xyz",
+repo: "https://github.com/hanzaladotjs/quickfire"
+        }
+    ]
     return (
         <div>
-pow
+            <AnimatedTestimonials testimonials={pow} light={light}/>
         </div>
     )
 }
